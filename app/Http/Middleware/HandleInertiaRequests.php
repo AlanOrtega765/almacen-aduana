8046b55642fd0d6fb5a2d6f1ca63070c2248f2de
AlanOrtega765 <alanortega765.webdeveloper@gmail.com>
2023-01-20 11:51:51 -0300

    Corregido error de datos del usuario globales

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -31,9 +31,9 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            'auth' => [
-                'user' => $request->user()->only('username'),
-            ],
+            'auth.user' => fn () => $request->user()
+                ? $request->user()->only('username')
+                : null,
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
                     'location' => $request->url(),
